2384731905ca08e764a51b38925b17d41c1fbc90
Virginia Wu <virginiawu11@gmail.com>
2021-11-09 15:32:17 -0500

    add skeleton routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import Header from "./ui/Header";
 import { ThemeProvider } from "@material-ui/styles";
-
 import theme from "./ui/Theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      hello!
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<div>Home</div>} />
+            <Route
+              exact
+              path="/customsoftware"
+              element={<div>customsoftware</div>}
+            />
+            <Route exact path="/mobileapps" element={<div>mobileapps</div>} />
+            <Route exact path="/websites" element={<div>websites</div>} />
+            <Route exact path="/revolution" element={<div>revolution</div>} />
+            <Route exact path="/about" element={<div>about</div>} />
+            <Route exact path="/contact" element={<div>contact</div>} />
+            <Route exact path="/estimate" element={<div>estimate</div>} />
+          </Routes>
+        </BrowserRouter>
+        <div>Hello</div>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>this is .</p>
+      <p>Isn't it beautiful?</p>
+    </div>
+  );
+}
+
+export default Home;
