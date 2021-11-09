c0e7b01c8f303ed3fa4b02a5d546f964fa0dea8b
Virginia Wu <virginiawu11@gmail.com>
2021-11-09 12:00:06 -0500

    fix appbar so it does not cover conent below

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 import Appbar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -15,14 +16,24 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <Appbar position="fixed" color="primary">
-        <Toolbar>
-          <Typography variant="h3">Arc Development</Typography>
-        </Toolbar>
-      </Appbar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <Appbar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h3">Arc Development</Typography>
+          </Toolbar>
+        </Appbar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
