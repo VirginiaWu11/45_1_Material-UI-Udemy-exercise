05ddae6ea43ca5b633cb6cf877d9d651402bdc9f
Virginia Wu <virginiawu11@gmail.com>
2021-11-09 12:19:04 -0500

    add logo to appbar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,8 +2,9 @@ import React from "react";
 import Appbar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Typography from "@material-ui/core/Typography";
+// import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -19,17 +20,23 @@ function ElevationScroll(props) {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
   },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+
   return (
     <>
       <ElevationScroll>
         <Appbar position="fixed" color="primary">
-          <Toolbar>
-            <Typography variant="h3">Arc Development</Typography>
+          <Toolbar disableGutters>
+            {/* <Typography variant="h3">Arc Development</Typography> */}
+            <img src={logo} alt="company logo" className={classes.logo} />
           </Toolbar>
         </Appbar>
       </ElevationScroll>
