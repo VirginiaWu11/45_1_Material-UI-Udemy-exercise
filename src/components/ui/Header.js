0e23cc112d1d45bc4bd8954bcb4c6aeb817b79b1
Virginia Wu <virginiawu11@gmail.com>
2021-11-09 13:25:00 -0500

    add formatting for tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,8 @@ import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 // import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
 import logo from "../../assets/logo.svg";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -25,6 +27,17 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "7em",
   },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    fontFamily: "Raleway",
+    textTransform: "none",
+    fontWeight: 700,
+    fontSize: "1rem",
+    minWidth: 10,
+    marginLeft: "25px",
+  },
 }));
 
 export default function Header(props) {
@@ -37,6 +50,13 @@ export default function Header(props) {
           <Toolbar disableGutters>
             {/* <Typography variant="h3">Arc Development</Typography> */}
             <img src={logo} alt="company logo" className={classes.logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </Appbar>
       </ElevationScroll>
